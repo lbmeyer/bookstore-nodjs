7d72379070ed351421c0da45e4a29a1e7c161c0e
Louis Wong <louiswong@Louiss-MBP.lan>
2019-01-06 14:56:51 -0500

    Added Controllers folder and moved routing logic into controllers

--- routes/shop.js
+++ routes/shop.js
@@ -2,21 +2,10 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
-const adminData = require('./admin');
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  const products = adminData.products;
-  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/',
-    activeShop: true,
-    productCSS: true
-  });
-});
+router.get('/', productsController.getProducts);
 
 module.exports = router;
