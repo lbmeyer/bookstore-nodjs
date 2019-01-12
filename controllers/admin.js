9b71121fb7ad12790a581c3fde44aa7d3d0a5c1f
Louis Wong <louiswong@Louiss-MBP.lan>
2019-01-12 12:02:42 -0500

    Added edit products functionality

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,12 +1,10 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
+  res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    formsCSS: true,
-    productsCSS: true,
-    activeAddProduct: true
+    editing: false
   });
 };
 
@@ -16,9 +14,50 @@ exports.postAddProduct = (req, res, next) => {
   const description = req.body.description;
   const price = req.body.price;
 
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null, title, imageUrl, description, price);
   product.save();
   res.redirect('/');
+};
+
+exports.getEditProduct = (req, res, next) => {
+  // query.edit will return a string of 'true' or 'false'
+  const editMode = req.query.edit;
+  console.log(editMode);
+  // if editMode = false, simply redirect
+  if (!editMode) {
+    console.log('being redirected...');
+    return res.redirect('/');
+  }
+
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    if (!product) {
+      return res.redirect('/');
+    }
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product: product
+    });
+  });
+};
+
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDesc = req.body.description;
+  const updatedProduct = new Product(
+    prodId,
+    updatedTitle,
+    updatedImageUrl,
+    updatedDesc,
+    updatedPrice
+  );
+  updatedProduct.save();
+  res.redirect('/admin/products');
 };
 
 exports.getProducts = (req, res, next) => {
@@ -29,4 +68,4 @@ exports.getProducts = (req, res, next) => {
       path: '/admin/products'
     });
   });
-}
+};
